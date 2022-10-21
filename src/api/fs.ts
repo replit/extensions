36612bdd1cb44abd2e3aa336757b8cca33d9203a
lunaroyster <oneinaquadrillion@live.com>
2022-10-20 17:35:57 -0700

    added types

--- src/api/fs.ts
+++ src/api/fs.ts
@@ -1,18 +1,18 @@
 import { extensionPort } from "src/util/comlink";
 import { request } from "src/util/talk";
 
-export async function readFile(path: string) {
+export async function readFile(path: string): Promise<{content: string}|{error: string}> {
   return extensionPort.fs.readFile(path);
 }
 
-export async function writeFile(path: string, content: string|Blob) {
+export async function writeFile(path: string, content: string|Blob): Promise<{success: boolean}|{error: string}> {
   return extensionPort.fs.writeFile(path, content);
 }
 
-export async function readDir(path: string) {
+export async function readDir(path: string): Promise<{children: Array<{filename: string, type: 'FILE'|'DIRECTORY'}>, error:string}> {
   return extensionPort.fs.readDir(path);
 }
 
-export async function createDir(path: string) {
+export async function createDir(path: string): Promise<null> {
   return extensionPort.fs.createDir(path);
 }
