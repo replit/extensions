95dd1e2e4856ccce80899b72b6a47c9d0caa3760
lunaroyster <oneinaquadrillion@live.com>
2022-11-26 16:51:31 +0530

    added signature plugin

---
+++ util/signature-plugin/index.js
@@ -0,0 +1,78 @@
+const { SignatureReflection, ReflectionKind, ReflectionType } = require("typedoc")
+
+// from https://github.com/TypeStrong/typedoc/issues/1662#issuecomment-907717438
+exports.load = function(app) {
+    app.serializer.addSerializer({
+        supports(x) {
+            return x instanceof SignatureReflection;
+        },
+        priority: 0,
+        toObject(signature, obj) {
+            // name of the function
+            const parts = [signature.name];
+
+            // adds `new` if it's a constructor
+            if (signature.kind === ReflectionKind.ConstructorSignature) {
+                if (signature.flags.isAbstract) parts.push("abstract ")
+                parts.push("new ")
+            }
+
+            // if it's a generic function, adds the type parameters
+            if (signature.typeParameters) {
+                parts.push("<")
+                let first = true
+                for (const typeParam of signature.typeParameters) {
+                    if (!first) parts.push(", ")
+                    parts.push(typeParam.name)
+                    if (typeParam.type) {
+                        parts.push(" extends ", typeParam.type.toString())
+                    }
+                    if (typeParam.default) {
+                        parts.push(" = ", typeParam.default.toString())
+                    }
+                    first = false
+                }
+                parts.push(">")
+            }
+
+            // adds the parameters
+            parts.push("(")
+            let first = true
+            for (const param of (signature.parameters || [])) {
+                if (!first) parts.push(", ")
+                parts.push(param.name, ": ", param.type.toString())
+                first = false
+            }
+            parts.push("): ")
+
+            // adds the return type
+            parts.push(signature.type.toString())
+
+            obj.stringifiedSignature = parts.join("")
+            return obj
+        }
+    });
+
+    // This is so we can stringify objects
+    app.serializer.addSerializer({
+        supports(x) {
+            return x instanceof ReflectionType;
+        },
+        priority: 1,
+        toObject: (x, obj) => {
+            let oldStringify = x.stringify;
+            x.stringify = () => {
+                if (!x.declaration.children) {
+                    if (oldStringify.call(x) === "Object") {
+                        // HACK: usually it's an empty object
+                        return '{}'
+                    }
+                    return oldStringify.call(x);
+                }
+
+                return `{ ${x.declaration.children.map(ch => `${ch.name}: ${ch.type.stringify()}`).join(", ")} }`;
+            }
+            return obj
+        }
+    })
+}
