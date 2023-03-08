0b3f9619ee6517e48277aa94d41978631b005013
IroncladDev <connerow@repl.it>
2023-03-08 16:15:53 -0600

    Add useWatchFile hook

---
+++ src/react/useWatchFile.ts
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { WatchTextFileWatchers } from "src/types";
+import useReplitCallback from "./useReplitCallback";
+
+interface Snapshot {
+  content: string;
+  version: number;
+}
+
+interface LatestSnapshot extends Snapshot {
+  changeSource: string;
+  changes: any; // TODO fix
+}
+
+export default function useTextFileWatcher(
+  filename: string,
+  watchers?: WatchTextFileWatchers
+) {
+  const [initial, setInitial] = useState<Snapshot | null>(null);
+  const [latest, setLatest] = useState<LatestSnapshot>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useReplitCallback(async ({ fs }) => {
+    await fs.watchTextFile(filename, {
+      onReady(data) {
+        setInitial({
+          content: data.initialContent,
+          version: data.version,
+        });
+        watchers?.onReady?.(data);
+      },
+      onChange(data) {
+        setLatest({
+          content: data.latestContent,
+          version: data.version,
+          changeSource: data.changeSource,
+          changes: data.changes,
+        });
+        watchers?.onChange?.(data);
+      },
+      onError(error) {
+        setError(error);
+        watchers?.onError?.(error);
+      },
+      onMoveOrDelete(data) {
+        watchers?.onMoveOrDelete?.(data);
+      },
+    });
+  }, []);
+
+  return { initial, latest, error };
+}
