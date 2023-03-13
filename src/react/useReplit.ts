330869f311c83c0d5bc5729653c42fb6f7640fe5
IroncladDev <connerow@repl.it>
2023-03-13 09:08:48 -0500

    Update src/react/useReplit.ts
    Co-authored-by: Arnav Bansal <lunaroyster@users.noreply.github.com>

--- src/react/useReplit.ts
+++ src/react/useReplit.ts
@@ -25,7 +25,7 @@ interface UseReplitFailure {
 /**
  * A React hook that initializes and passes the Replit API wrapper to a component.
  */
-export default function useReplit(init?: { permissions: Array<string> }) {
+export default function useReplit(args?: { permissions: Array<string> }) {
   const [status, setStatus] = useState<"loading" | "error" | "ready">(
     "loading"
   );
