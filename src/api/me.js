aa04a26daf6ef1f71d0e407ccefeaa5d09ec024a
IroncladDev <connerow@repl.it>
2023-03-15 13:55:32 -0500

    fix typing

--- src/api/me.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filePath = void 0;
-const src_1 = require("src");
-function filePath() {
-    return src_1.extensionPort.filePath;
-}
-exports.filePath = filePath;
