c54bb212d2841deea440510cae121ed6bd77a842
IroncladDev <connerow@repl.it>
2023-03-15 15:29:50 -0500

    comments

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,9 +1,15 @@
 import { extensionPort } from "src/util/comlink";
 
+/**
+ * Exposes the user API wrapper
+ */
 export async function user() {
   return await extensionPort.user;
 }
 
+/**
+ * Exposes the repl API wrapper
+ */
 export async function repl() {
   return await extensionPort.repl;
 }
