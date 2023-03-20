68c7fa0b044342737b63a9155e7340c91e96935e
IroncladDev <connerow@repl.it>
2023-03-20 16:18:30 -0500

    Notification API

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,9 @@ import * as fs from "./fs";
 import * as replDb from "./replDb";
 import * as me from "./me";
 import * as theme from "./theme";
+import * as messages from "./messages";
 
-export { fs, replDb, me, theme };
+export { fs, replDb, me, theme, messages };
 
 // deprecate this after migrating existing extensions
 export * from "./fs";
