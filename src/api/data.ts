91f3a2d18740771b32abb3553a1041a4d07fe1bc
IroncladDev <connerow@repl.it>
2023-03-23 17:03:08 -0500

    fix types and expose to apis.json

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,30 +1,36 @@
-import { UserDataInclusion } from "src/types";
+import {
+  JsonDataReponse,
+  ReplDataInclusion,
+  UserDataInclusion,
+} from "src/types";
 import { extensionPort } from "src/util/comlink";
 
 export const user = {
-  async current(args: UserDataInclusion) {
+  async current(args: UserDataInclusion): JsonDataReponse {
     return await extensionPort.currentUser(args);
   },
 
-  async byId(args: { id: string } & UserDataInclusion) {
+  async byId(args: { id: string } & UserDataInclusion): JsonDataReponse {
     return await extensionPort.userById(args);
   },
 
-  async byUsername(args: { username: string } & UserDataInclusion) {
+  async byUsername(
+    args: { username: string } & UserDataInclusion
+  ): JsonDataReponse {
     return await extensionPort.userByUsername(args);
   },
 };
 
 export const repl = {
-  async current(args: UserDataInclusion) {
+  async current(args: ReplDataInclusion): JsonDataReponse {
     return await extensionPort.currentRepl(args);
   },
 
-  async byId(args: { id: string } & UserDataInclusion) {
+  async byId(args: { id: string } & ReplDataInclusion): JsonDataReponse {
     return await extensionPort.replById(args);
   },
 
-  async byUrl(args: { url: string } & UserDataInclusion) {
+  async byUrl(args: { url: string } & ReplDataInclusion): JsonDataReponse {
     return await extensionPort.replByUrl(args);
   },
 };
