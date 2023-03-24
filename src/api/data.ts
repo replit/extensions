199612a4d8d9a36fc1cd0011d10b419ee967dfcf
IroncladDev <connerow@repl.it>
2023-03-24 07:31:53 -0500

    docstrings

--- src/api/data.ts
+++ src/api/data.ts
@@ -5,32 +5,50 @@ import {
 } from "src/types";
 import { extensionPort } from "src/util/comlink";
 
+/**
+ * Fetches the current user via graphql
+ */
 export async function currentUser(args: UserDataInclusion): JsonDataReponse {
   return await extensionPort.currentUser(args);
 }
 
+/**
+ * Fetches a user by their id via graphql
+ */
 export async function userById(
   args: { id: string } & UserDataInclusion
 ): JsonDataReponse {
   return await extensionPort.userById(args);
 }
 
+/**
+ * Fetches a user by their username via graphql
+ */
 export async function userByUsername(
   args: { username: string } & UserDataInclusion
 ): JsonDataReponse {
   return await extensionPort.userByUsername(args);
 }
 
+/**
+ * Fetches the current Repl via graphql
+ */
 export async function currentRepl(args: ReplDataInclusion): JsonDataReponse {
   return await extensionPort.currentRepl(args);
 }
 
+/**
+ * Fetches a Repl by its ID via graphql
+ */
 export async function replById(
   args: { id: string } & ReplDataInclusion
 ): JsonDataReponse {
   return await extensionPort.replById(args);
 }
 
+/**
+ * Fetches a Repl by its URL via graphql
+ */
 export async function replByUrl(
   args: { url: string } & ReplDataInclusion
 ): JsonDataReponse {
