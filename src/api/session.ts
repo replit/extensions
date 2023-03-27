413c7e393c9dc95b5ae525607986fa84df62cabc
IroncladDev <connerow@repl.it>
2023-03-27 17:25:57 -0500

    boop

--- src/api/session.ts
+++ src/api/session.ts
@@ -5,7 +5,7 @@ import { extensionPort, proxy } from "src";
  */
 export function onActiveFileChange(
   callback: (file: string) => void
-): VoidFunction {
+) {
   let dispose = () => {
     console.log("disposing existing watcher");
   };
