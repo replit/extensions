6caa2d2074bcda766f950986d1ecdb24649aaeaa
IroncladDev <connerow@repl.it>
2023-03-27 10:33:12 -0500

    Organize types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,12 @@
-export type Pane = {
-  type: string;
-  id: string;
-};
+/*****************************************************************
+ * * FS (Filesystem) Types
+ *****************************************************************/
 
 /**
- * Enumeration of file types.
+ * A Filesystem node type
  */
-export enum FileType {
-  /**
-   * A regular file.
-   */
+export enum FsNodeType {
   File = "FILE",
-  /**
-   * A directory/folder
-   */
   Directory = "DIRECTORY",
 }
 
@@ -23,15 +16,8 @@ export enum FileType {
  * does not expose the node's content/children
  */
 export interface FsNode {
-  /**
-   * Full path of the node relative to the root
-   */
   path: string;
-
-  /**
-   * node is a file
-   */
-  type: FileType;
+  type: FsNodeType;
 }
 
 export interface WatchFileWatchers {
@@ -68,159 +54,112 @@ export interface WatchTextFileWatchers {
   }) => void;
 }
 
-/**
- * A unique id for a pane in the layout
- */
-export type PaneId = string;
-
-/**
- * A unique id for a pane group in the layout
- */
-export type PaneGroupId = string;
-
-/**
- * Represents the Repl's layout and pane data in a serializable manner
- */
-export interface LayoutData {
-  layout: {
-    floating: Array<FloatingPaneGroup>;
-    tiling: any;
-  };
-  data: Record<string, any>;
-  sidebarPercent: number;
+export interface DirectoryChildNode {
+  filename: string;
+  type: FsNodeType;
 }
 
-/**
- * The size in pixles within the layout
- */
-export interface Size {
-  width: number;
-  height: number;
-}
-
-/**
- * The position in pixles within the layout
- */
-export interface Point {
-  x: number;
-  y: number;
-}
-
-/**
- * Size and position in pixles within the layout
- */
-export interface Rect extends Point, Size {}
-
-/**
- * A floating group of panes
- */
-export interface FloatingPaneGroup {
-  id: string;
-  type: "floatingPaneGroup";
-  panes: Array<PaneId>;
-  activeIndex: number;
-  rect: Rect;
-}
-
-export type CssColor = string;
+/*****************************************************************
+ * * Theme Types
+ *****************************************************************/
 
 export interface ThemeValues {
   global: {
-    backgroundRoot: CssColor;
-    backgroundDefault: CssColor;
-    backgroundHigher: CssColor;
-    backgroundHighest: CssColor;
-    backgroundOverlay: CssColor;
-    foregroundDefault: CssColor;
-    foregroundDimmer: CssColor;
-    foregroundDimmest: CssColor;
-    outlineDimmest: CssColor;
-    outlineDimmer: CssColor;
-    outlineDefault: CssColor;
-    outlineStronger: CssColor;
-    outlineStrongest: CssColor;
-    accentPrimaryDimmest: CssColor;
-    accentPrimaryDimmer: CssColor;
-    accentPrimaryDefault: CssColor;
-    accentPrimaryStronger: CssColor;
-    accentPrimaryStrongest: CssColor;
-    accentPositiveDimmest: CssColor;
-    accentPositiveDimmer: CssColor;
-    accentPositiveDefault: CssColor;
-    accentPositiveStronger: CssColor;
-    accentPositiveStrongest: CssColor;
-    accentNegativeDimmest: CssColor;
-    accentNegativeDimmer: CssColor;
-    accentNegativeDefault: CssColor;
-    accentNegativeStronger: CssColor;
-    accentNegativeStrongest: CssColor;
-    redDimmest: CssColor;
-    redDimmer: CssColor;
-    redDefault: CssColor;
-    redStronger: CssColor;
-    redStrongest: CssColor;
-    orangeDimmest: CssColor;
-    orangeDimmer: CssColor;
-    orangeDefault: CssColor;
-    orangeStronger: CssColor;
-    orangeStrongest: CssColor;
-    yellowDimmest: CssColor;
-    yellowDimmer: CssColor;
-    yellowDefault: CssColor;
-    yellowStronger: CssColor;
-    yellowStrongest: CssColor;
-    limeDimmest: CssColor;
-    limeDimmer: CssColor;
-    limeDefault: CssColor;
-    limeStronger: CssColor;
-    limeStrongest: CssColor;
-    greenDimmest: CssColor;
-    greenDimmer: CssColor;
-    greenDefault: CssColor;
-    greenStronger: CssColor;
-    greenStrongest: CssColor;
-    tealDimmest: CssColor;
-    tealDimmer: CssColor;
-    tealDefault: CssColor;
-    tealStronger: CssColor;
-    tealStrongest: CssColor;
-    blueDimmest: CssColor;
-    blueDimmer: CssColor;
-    blueDefault: CssColor;
-    blueStronger: CssColor;
-    blueStrongest: CssColor;
-    blurpleDimmest: CssColor;
-    blurpleDimmer: CssColor;
-    blurpleDefault: CssColor;
-    blurpleStronger: CssColor;
-    blurpleStrongest: CssColor;
-    purpleDimmest: CssColor;
-    purpleDimmer: CssColor;
-    purpleDefault: CssColor;
-    purpleStronger: CssColor;
-    purpleStrongest: CssColor;
-    magentaDimmest: CssColor;
-    magentaDimmer: CssColor;
-    magentaDefault: CssColor;
-    magentaStronger: CssColor;
-    magentaStrongest: CssColor;
-    pinkDimmest: CssColor;
-    pinkDimmer: CssColor;
-    pinkDefault: CssColor;
-    pinkStronger: CssColor;
-    pinkStrongest: CssColor;
-    greyDimmest: CssColor;
-    greyDimmer: CssColor;
-    greyDefault: CssColor;
-    greyStronger: CssColor;
-    greyStrongest: CssColor;
-    brownDimmest: CssColor;
-    brownDimmer: CssColor;
-    brownDefault: CssColor;
-    brownStronger: CssColor;
-    brownStrongest: CssColor;
-    black: CssColor;
-    white: CssColor;
+    backgroundRoot: string;
+    backgroundDefault: string;
+    backgroundHigher: string;
+    backgroundHighest: string;
+    backgroundOverlay: string;
+    foregroundDefault: string;
+    foregroundDimmer: string;
+    foregroundDimmest: string;
+    outlineDimmest: string;
+    outlineDimmer: string;
+    outlineDefault: string;
+    outlineStronger: string;
+    outlineStrongest: string;
+    accentPrimaryDimmest: string;
+    accentPrimaryDimmer: string;
+    accentPrimaryDefault: string;
+    accentPrimaryStronger: string;
+    accentPrimaryStrongest: string;
+    accentPositiveDimmest: string;
+    accentPositiveDimmer: string;
+    accentPositiveDefault: string;
+    accentPositiveStronger: string;
+    accentPositiveStrongest: string;
+    accentNegativeDimmest: string;
+    accentNegativeDimmer: string;
+    accentNegativeDefault: string;
+    accentNegativeStronger: string;
+    accentNegativeStrongest: string;
+    redDimmest: string;
+    redDimmer: string;
+    redDefault: string;
+    redStronger: string;
+    redStrongest: string;
+    orangeDimmest: string;
+    orangeDimmer: string;
+    orangeDefault: string;
+    orangeStronger: string;
+    orangeStrongest: string;
+    yellowDimmest: string;
+    yellowDimmer: string;
+    yellowDefault: string;
+    yellowStronger: string;
+    yellowStrongest: string;
+    limeDimmest: string;
+    limeDimmer: string;
+    limeDefault: string;
+    limeStronger: string;
+    limeStrongest: string;
+    greenDimmest: string;
+    greenDimmer: string;
+    greenDefault: string;
+    greenStronger: string;
+    greenStrongest: string;
+    tealDimmest: string;
+    tealDimmer: string;
+    tealDefault: string;
+    tealStronger: string;
+    tealStrongest: string;
+    blueDimmest: string;
+    blueDimmer: string;
+    blueDefault: string;
+    blueStronger: string;
+    blueStrongest: string;
+    blurpleDimmest: string;
+    blurpleDimmer: string;
+    blurpleDefault: string;
+    blurpleStronger: string;
+    blurpleStrongest: string;
+    purpleDimmest: string;
+    purpleDimmer: string;
+    purpleDefault: string;
+    purpleStronger: string;
+    purpleStrongest: string;
+    magentaDimmest: string;
+    magentaDimmer: string;
+    magentaDefault: string;
+    magentaStronger: string;
+    magentaStrongest: string;
+    pinkDimmest: string;
+    pinkDimmer: string;
+    pinkDefault: string;
+    pinkStronger: string;
+    pinkStrongest: string;
+    greyDimmest: string;
+    greyDimmer: string;
+    greyDefault: string;
+    greyStronger: string;
+    greyStrongest: string;
+    brownDimmest: string;
+    brownDimmer: string;
+    brownDefault: string;
+    brownStronger: string;
+    brownStrongest: string;
+    black: string;
+    white: string;
   };
   editor: {
     //   syntaxHighlighting: Array<SyntaxHighlightingSpec>;
@@ -242,69 +181,9 @@ export interface Theme {
   isOfficial: boolean;
 }
 
-export type ExtensionPortAPI = {
-  // init Function
-  handshake: () => void;
-
-  // fs Module
-  readFile: (
-    path: string,
-    encoding: "utf8" | "binary" | null
-  ) => Promise<{ content: string } | { error: string }>;
-  writeFile: (
-    path: string,
-    content: string | Blob
-  ) => Promise<{ success: boolean } | { error: string }>;
-  readDir: (path: string) => Promise<{
-    children: Array<{ filename: string; type: "FILE" | "DIRECTORY" }>;
-    error: string;
-  }>;
-  createDir: (path: string) => Promise<{} | { error: string }>;
-  deleteFile: (path: string) => Promise<{} | { error: string }>;
-  deleteDir: (path: string) => Promise<{} | { error: string }>;
-  move: (path: string, to: string) => Promise<{ error: string | null }>;
-  copyFile: (path: string, to: string) => Promise<{ error: string | null }>;
-  watchFile: (path: string, watcher: WatchFileWatchers) => () => void;
-  watchTextFile: (path: string, watcher: WatchTextFileWatchers) => () => void;
-
-  // replDb Module
-  setReplDbValue: (key: string, value: string) => Promise<void>;
-  getReplDbValue: (key: string) => Promise<string | null>;
-  listReplDbKeys: (
-    prefix: string
-  ) => Promise<{ keys: string[] } | { error: string }>;
-  deleteReplDbKey: (key: string) => Promise<void>;
-
-  activatePane: () => Promise<void>;
-
-  // theme
-  getCurrentTheme: () => Promise<Theme>;
-  onThemeChange: (callback: (theme: Theme) => void) => Promise<() => void>;
-
-  filePath: string;
-
-  // messages Module
-  showConfirm: (text: string, length?: number) => string;
-  showError: (text: string, length?: number) => string;
-  showNotice: (text: string, length?: number) => string;
-  showWarning: (text: string, length?: number) => string;
-  hideMessage: (id: string) => void;
-  hideAllMessages: () => void;
-
-  // data Module
-  currentUser: (args: UserDataInclusion) => UserQueryOutput;
-  userById: (args: { id: string } & UserDataInclusion) => UserQueryOutput;
-  userByUsername: (
-    args: { username: string } & UserDataInclusion
-  ) => UserByUsernameQueryOutput;
-  currentRepl: (args: ReplDataInclusion) => ReplQueryOutput;
-  replById: (args: { id: string } & ReplDataInclusion) => ReplQueryOutput;
-  replByUrl: (args: { url: string } & ReplDataInclusion) => ReplQueryOutput;
-
-  // session Module
-  watchActiveFile: (callback: (path: string) => void) => () => void;
-  getActiveFile: () => Promise<string | null>;
-};
+/*****************************************************************
+ * * Data Input/Output Types
+ *****************************************************************/
 
 export interface UserDataInclusion {
   includeSocialData?: boolean;
@@ -318,12 +197,17 @@ export interface ReplDataInclusion {
   includeMultiplayers?: boolean;
 }
 
-export enum HandshakeStatus {
-  Ready = "ready",
-  Error = "error",
-  Loading = "loading",
-}
+export type GraphResponse<T> = Promise<T | never>;
 
+export type ReplQueryOutput = GraphResponse<{ repl: Repl }>;
+export type UserByUsernameQueryOutput = GraphResponse<{ userByUsername: User }>;
+export type UserQueryOutput = GraphResponse<{ user: User }>;
+
+/*****************************************************************
+ * * GraphQL Types
+ *****************************************************************/
+
+// User
 export interface User {
   id: number;
   username: string;
@@ -341,7 +225,7 @@ export interface User {
   followerCount?: number;
 
   // RolesUserData fragment
-  roles?: UserRole;
+  roles?: Array<UserRole>;
 }
 
 export interface UserSocial {
@@ -349,7 +233,6 @@ export interface UserSocial {
   url: string;
   type: UserSocialType;
 }
-
 export enum UserSocialType {
   twitter = "twitter",
   github = "github",
@@ -360,7 +243,6 @@ export enum UserSocialType {
   facebook = "facebook",
   discord = "discord",
 }
-
 export interface UserRole {
   id: number;
   name: string;
@@ -368,6 +250,7 @@ export interface UserRole {
   tagline: string;
 }
 
+// Repl
 export interface Repl {
   id: string;
   url: string;
@@ -407,25 +290,88 @@ export interface Tag {
   id: string;
   isOfficial: boolean;
 }
-
 export interface ReplComment {
   id: number;
   body: string;
   user: User;
 }
 
-// Response types
+/*****************************************************************
+ * * Miscalleneous / React Types
+ *****************************************************************/
 
-export type GraphResponse<T> = Promise<T | never>;
+export enum HandshakeStatus {
+  Ready = "ready",
+  Error = "error",
+  Loading = "loading",
+}
 
-export type ReplQueryOutput = GraphResponse<{
-  repl: Repl;
-}>;
+export type StrError = { error: string };
+export type NullableStrError = { error: string | null };
+export type VoidPromise = Promise<void>;
+export type VoidFunction = () => void;
 
-export type UserByUsernameQueryOutput = GraphResponse<{
-  userByUsername: User;
-}>;
+/*****************************************************************
+ * * Extension Port Wrapper
+ *****************************************************************/
 
-export type UserQueryOutput = GraphResponse<{
-  user: User;
-}>;
+export type ExtensionPortAPI = {
+  handshake: () => void;
+
+  // fs Module
+  readFile: (
+    path: string,
+    encoding: "utf8" | "binary" | null
+  ) => Promise<{ content: string } | StrError>;
+  writeFile: (
+    path: string,
+    content: string | Blob
+  ) => Promise<{ success: boolean } | StrError>;
+  readDir: (path: string) => Promise<{
+    children: Array<DirectoryChildNode>;
+    error: string;
+  }>;
+  createDir: (path: string) => Promise<{} | StrError>;
+  deleteFile: (path: string) => Promise<{} | StrError>;
+  deleteDir: (path: string) => Promise<{} | StrError>;
+  move: (path: string, to: string) => Promise<NullableStrError>;
+  copyFile: (path: string, to: string) => Promise<NullableStrError>;
+  watchFile: (path: string, watcher: WatchFileWatchers) => VoidFunction;
+  watchTextFile: (path: string, watcher: WatchTextFileWatchers) => VoidFunction;
+
+  // replDb Module
+  setReplDbValue: (key: string, value: string) => VoidPromise;
+  getReplDbValue: (key: string) => NullableStrError;
+  listReplDbKeys: (prefix: string) => Promise<{ keys: string[] } | StrError>;
+  deleteReplDbKey: (key: string) => VoidPromise;
+
+  activatePane: () => VoidPromise;
+
+  // theme
+  getCurrentTheme: () => Promise<Theme>;
+  onThemeChange: (callback: (theme: Theme) => void) => Promise<() => void>;
+
+  filePath: string;
+
+  // messages Module
+  showConfirm: (text: string, length?: number) => string;
+  showError: (text: string, length?: number) => string;
+  showNotice: (text: string, length?: number) => string;
+  showWarning: (text: string, length?: number) => string;
+  hideMessage: (id: string) => void;
+  hideAllMessages: () => void;
+
+  // data Module
+  currentUser: (args: UserDataInclusion) => UserQueryOutput;
+  userById: (args: { id: string } & UserDataInclusion) => UserQueryOutput;
+  userByUsername: (
+    args: { username: string } & UserDataInclusion
+  ) => UserByUsernameQueryOutput;
+  currentRepl: (args: ReplDataInclusion) => ReplQueryOutput;
+  replById: (args: { id: string } & ReplDataInclusion) => ReplQueryOutput;
+  replByUrl: (args: { url: string } & ReplDataInclusion) => ReplQueryOutput;
+
+  // session Module
+  watchActiveFile: (callback: (path: string) => void) => () => void;
+  getActiveFile: () => Promise<string | null>;
+};
