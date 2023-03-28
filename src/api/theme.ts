9e327b916825a70854d1800f8c3fb352eed6d23b
IroncladDev <connerow@repl.it>
2023-03-28 09:24:08 -0500

    document

--- src/api/theme.ts
+++ src/api/theme.ts
@@ -1,10 +1,14 @@
 import { proxy } from "comlink";
-import { extensionPort, ThemeValuesGlobal } from "src";
+import {
+  extensionPort,
+  OnThemeChangeValuesCallback,
+  ThemeValuesGlobal,
+} from "src";
 
 /**
  * Returns the current theme's global token values.
  */
-export async function getCurrentThemeValues() {
+export async function getCurrentThemeValues(): Promise<ThemeValuesGlobal> {
   return await extensionPort.getCurrentThemeValues();
 }
 
@@ -12,7 +16,7 @@ export async function getCurrentThemeValues() {
  * Fires the `callback` parameter function with the updated theme values when the theme changes.
  */
 export async function onThemeChangeValues(
-  callback: (theme: ThemeValuesGlobal) => void
+  callback: OnThemeChangeValuesCallback
 ) {
   return await extensionPort.onThemeChangeValues(proxy(callback));
 }
