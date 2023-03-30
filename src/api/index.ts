7fe9fcbcd9398dacf4daa8eab9edd713cc9051a3
IroncladDev <connerow@repl.it>
2023-03-30 07:07:55 -0500

    misspelled name

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,12 @@
 import * as fs from "./fs";
 import * as replDb from "./replDb";
 import * as me from "./me";
-import * as theme from "./theme";
+import * as themes from "./theme";
 import * as messages from "./messages";
 import * as data from "./data";
 import * as session from "./session";
 
-export { fs, replDb, me, theme, messages, data, session };
+export { fs, replDb, me, themes, messages, data, session };
 
 // deprecate this after migrating existing extensions
 export * from "./fs";
