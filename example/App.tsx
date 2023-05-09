dbd2e394b716e12b941e9152f77827564c8185f2
Arnav Bansal <lunaroyster@users.noreply.github.com>
2023-05-09 19:45:13 +0530

    Add encoding option to watchFile (#55)
    * added encoding to watchFile
    
    * removed auth (wrong pr)
    
    * linted

--- example/App.tsx
+++ example/App.tsx
@@ -1,47 +1,15 @@
 import * as React from "react";
 import { useReplit, useWatchTextFile } from "@replit/extensions-react";
-import { messages, UseWatchTextFileStatus } from "@replit/extensions";
+import * as replit from "@replit/extensions";
 import "./App.css";
+const { messages, HandshakeStatus } = replit;
 
 export default function App() {
   const { status, error, filePath } = useReplit();
 
-  const connected: boolean = status === "ready";
-
-  const {
-    content,
-    watchError,
-    writeChange,
-    status: watchStatus,
-  } = useWatchTextFile({
-    filePath: "example/test.json",
-  });
-
-  const sendMessage = () => {
-    messages.showNotice("THIS IS A TEST");
-  };
-
-  const randomizeJson = async () => {
-    try {
-      const json = JSON.parse(content || "{}");
-
-      const randomKey =
-        Object.keys(json)[Math.floor(Math.random() * Object.keys(json).length)];
-
-      json[randomKey] = Math.random();
-
-      if (writeChange) {
-        writeChange({
-          from: 0,
-          to: content?.length || 0,
-          insert: JSON.stringify(json, null, 2),
-        });
-      }
-    } catch (e) {
-      console.error(e);
-      await messages.showError("Error randomizing JSON");
-    }
-  };
+  if (typeof window !== "undefined") {
+    window.replit = replit;
+  }
 
   return (
     <main>
@@ -60,20 +28,11 @@ export default function App() {
             </>
           ) : (
             <div>
-              {connected
+              {status === HandshakeStatus.Ready
                 ? filePath
                   ? `connected to ${filePath}`
                   : "connected"
                 : "connecting..."}
-              <button onClick={sendMessage}>Click</button>
-              <button onClick={randomizeJson}>Randomize JSON</button>
-              <hr />
-              {watchStatus === UseWatchTextFileStatus.Watching
-                ? "watching"
-                : "not watching " + watchError}{" "}
-              | {filePath}
-              <hr />
-              <pre>{content}</pre>
             </div>
           )}
           <span>{status}</span>
--- src/api/fs/index.ts
+++ src/api/fs/index.ts
@@ -68,7 +68,11 @@ export async function copyFile(path: string, to: string) {
 /**
  * Watches the file at `path` for changes with the provided `listeners`. Returns a dispose method which cleans up the listeners
  */
-export async function watchFile(path: string, listeners: WatchFileListeners) {
+export async function watchFile(
+  path: string,
+  listeners: WatchFileListeners,
+  encoding: "utf8" | "binary" = "binary"
+) {
   // Note: comlink does not let us test for functions being present, so we provide default functions for all callbacks in case the user does not pass those, to keep the API flexible
   return extensionPort.watchFile(
     path,
@@ -76,7 +80,8 @@ export async function watchFile(path: string, listeners: WatchFileListeners) {
       onMoveOrDelete: () => {},
       onError: () => {},
       ...listeners,
-    })
+    }),
+    encoding
   );
 }
 
