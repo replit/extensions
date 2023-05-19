48186014b10f0e09a718bd81a3d94dc215c9d3e1
IroncladDev <connerow@repl.it>
2023-05-19 12:37:56 -0500

    Update modules/extensions/src/api/internal/auth.ts
    Co-authored-by: Arnav Bansal <lunaroyster@users.noreply.github.com>

--- modules/extensions/src/api/internal/auth.ts
+++ modules/extensions/src/api/internal/auth.ts
@@ -2,7 +2,7 @@ import { extensionPort } from "../../util/comlink";
 import * as jose from "jose";
 
 /**
- * Returns a unique JWT token that can be used to authenticate with Replit's API.
+ * Returns a unique JWT token that can be used to verify that an extension has been loaded on Replit by a particular user
  */
 export async function getAuthToken() {
   return extensionPort.internal.auth.getAuthToken();
