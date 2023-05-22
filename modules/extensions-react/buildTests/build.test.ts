03af301e8f5d8099b8fde65b4aa780158c9c2ea7
IroncladDev <connerow@repl.it>
2023-05-22 16:51:05 -0500

    Tests, tests everywhere

---
+++ modules/extensions-react/buildTests/build.test.ts
@@ -0,0 +1,25 @@
+const fs = require("fs")
+
+describe("dist/index.cjs (CommonJS)", () => {
+  test("exists", () => {
+    expect(fs.existsSync("./dist/index.cjs")).toBe(true);
+  });
+  test("sourcemap file exists", () => {
+    expect(fs.existsSync("./dist/index.cjs.map")).toBe(true);
+  });
+});
+
+describe("dist/index.js (ES Module)", () => {
+  test("exists", () => {
+    expect(fs.existsSync("./dist/index.js")).toBe(true);
+  });
+  test("sourcemap file exists", () => {
+    expect(fs.existsSync("./dist/index.js.map")).toBe(true);
+  });
+});
+
+describe("dist/index.d.ts (TypeScript defs)", () => {
+  test("exists", () => {
+    expect(fs.existsSync("./dist/index.d.ts")).toBe(true);
+  });
+});
