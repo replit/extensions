a307bc82fad9aa18748b3890171300e34ad344ff
IroncladDev <connerow@repl.it>
2023-05-24 11:23:34 -0500

    Unexperimentalize exec() (WIP)

---
+++ modules/extensions/src/api/exec.ts
@@ -0,0 +1,69 @@
+import { extensionPort, proxy } from "../util/comlink";
+import {
+  CombinedOutputExecOptions,
+  CombinedOutputExecResult,
+  SeparatedOutputExecOptions,
+  SeparatedOutputExecResult,
+} from "../types";
+
+/**
+ * Executes arbitrary shell commands, with given arguments and environment variables
+ */
+export async function exec(
+  combinedOutputOptions: CombinedOutputExecOptions
+): Promise<CombinedOutputExecResult>;
+export async function exec(
+  separatedOutputOptions: SeparatedOutputExecOptions
+): Promise<SeparatedOutputExecResult>;
+export async function exec(
+  options: CombinedOutputExecOptions | SeparatedOutputExecOptions
+): Promise<SeparatedOutputExecResult | CombinedOutputExecResult> {
+  let outputStr: string = "";
+  let errorStr: string = "";
+  let exitCode: string = "";
+
+  const { promise } = await extensionPort.experimental.exec(
+    proxy({
+      args: Array.isArray(options.args)
+        ? options.args
+        : ["bash", "-c", options.args],
+      env: options.env || {},
+      splitStderr: options.separateStdErr || false,
+      onOutput: (output: string) => {
+        outputStr += output;
+        if (options.separateStdErr) {
+          options.onStdOutOutput?.(output);
+        } else {
+          options.onOutput?.(output);
+        }
+      },
+      onStdErr: (stderr: string) => {
+        if (options.separateStdErr) {
+          errorStr += stderr;
+          options.onStdErrOutput?.(stderr);
+        } else {
+          outputStr += stderr;
+          options.onOutput?.(stderr);
+        }
+      },
+      onError: (err: Error) => {
+        throw err;
+      },
+    })
+  );
+
+  await promise;
+
+  if (options.separateStdErr) {
+    return {
+      output: outputStr,
+      error: errorStr,
+      exitError: exitCode || null,
+    };
+  } else {
+    return {
+      output: outputStr,
+      exitError: exitCode || null,
+    };
+  }
+}
