8798a78dd69e75bc0f82c3ed604717becf6708da
IroncladDev <connerow@repl.it>
2023-05-31 14:27:54 -0500

    boop

--- modules/dev/src/tests/exec.ts
+++ modules/dev/src/tests/exec.ts
@@ -6,9 +6,7 @@ const { exec } = experimental;
 
 const tests: TestObject = {
   exec: async () => {
-    await exec({
-      args: ["touch", "test_text_file.txt"],
-    });
+    await exec.exec("touch test_text_file.txt");
     const res = await fs.readFile("test_text_file.txt");
 
     assert.isObject(res);
