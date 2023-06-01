a2c38646d53e77cb5bb61bde19ecb88aa056fea3
Conner1115 <connerow@repl.it>
2023-06-01 21:32:28 +0000

    exec stuff

--- modules/dev/src/tests/exec.ts
+++ modules/dev/src/tests/exec.ts
@@ -1,24 +1,35 @@
 import { TestNamespace, TestObject } from "../types";
 import { experimental, fs } from "@replit/extensions";
 import { assert } from "chai";
+import { assertFileExists, createTestDirIfNotExists } from "../utils/tests";
 
 const { exec } = experimental;
 
 const tests: TestObject = {
-  exec: async () => {
-    await exec.exec("touch test_text_file.txt");
-    const res = await fs.readFile("test_text_file.txt");
+  "exec should run a bash command": async () => {
+    await createTestDirIfNotExists();
 
-    assert.isObject(res);
+    await exec.exec("touch extension_tester/test_text_file.txt");
+    const res = await assertFileExists("extension_tester/test_text_file.txt");
 
-    if ("content" in res) {
-      assert.isString(res.content);
-    } else {
-      throw new Error("Expected a string");
-    }
+    assert.isString(res.content);
 
     // Cleanup
-    await fs.deleteFile("test_text_file.txt");
+    await fs.deleteFile("extension_tester/test_text_file.txt");
+  },
+  "spawn should successfully spawn a connection instance": async (log) => {
+    const { resultPromise, dispose } = exec.spawn({
+      args: ["echo", "hello"],
+      onOutput: (output) => {
+        assert.isString(output);
+        log("> " + output);
+      },
+    });
+
+    await resultPromise;
+
+    // Cleanup
+    dispose();
   },
 };
 
--- replit.nix
+++ replit.nix
@@ -1,6 +1,5 @@
 { pkgs }: {
   deps = [
-		pkgs.nodePackages.prettier
     pkgs.nodejs-18_x
     pkgs.nodePackages.typescript-language-server
     pkgs.yarn
