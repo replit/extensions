5e47ad6aad8c89069337ee5bfea9a41ca4aae166
Conner1115 <connerow@repl.it>
2023-06-02 14:45:42 +0000

    ALL THE THINGS

--- modules/dev/src/tests/data.ts
+++ modules/dev/src/tests/data.ts
@@ -1,14 +1,59 @@
 import { TestNamespace, TestObject } from "../types";
 import { data } from "@replit/extensions";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 const tests: TestObject = {
-  currentUser: async () => {
+  "currentUser gets the current user": async (log) => {
     const res = await data.currentUser();
 
     assert.isObject(res.user);
+    assert.isNumber(res.user.id);
 
-    assert.isTrue(typeof res.user.id === "number");
+    log("Current User: " + res.user.username);
+  },
+  "userById fetches a Replit user by their ID": async () => {
+    const res = await data.userById({
+      id: 1,
+    });
+
+    assert.isObject(res.user);
+    expect(res.user.username).to.equal("amasad");
+  },
+  "userByUsername fetches a Replit user by their username": async () => {
+    const res = await data.userByUsername({
+      username: "amasad",
+    });
+
+    assert.isObject(res.userByUsername);
+    expect(res.userByUsername.username).to.equal("amasad");
+  },
+  "currentRepl gets fetches the current Repl": async (log) => {
+    const res = await data.currentRepl({
+      includeOwner: true,
+    });
+
+    assert.isObject(res.repl);
+    assert.isString(res.repl.id);
+
+    log("Repl: " + res.repl.title + " by @" + res.repl.owner?.username);
+  },
+  "replById fetches a Repl by its ID": async () => {
+    // Moderation Welcome Repl
+    const id = "caf1f00a-5184-4670-bc97-683f9cc337e0";
+
+    const res = await data.replById({ id });
+
+    assert.isObject(res.repl);
+    assert.isString(res.repl.id);
+    assert.isString(res.repl.title);
+  },
+  "replByUrl fetches a Repl by its URL": async () => {
+    const url = "/@moderation/Welcome";
+    const res = await data.replByUrl({ url });
+
+    assert.isObject(res.repl);
+    assert.isString(res.repl.id);
+    assert.isString(res.repl.title);
   },
 };
 
--- modules/dev/src/tests/themes.ts
+++ modules/dev/src/tests/themes.ts
@@ -3,11 +3,52 @@ import { themes } from "@replit/extensions";
 import { assert } from "chai";
 
 const tests: TestObject = {
-  getCurrentTheme: async () => {
+  "getCurrentTheme gets the current theme": async (log) => {
     const res = await themes.getCurrentTheme();
 
     assert.isObject(res);
     assert.isNumber(res.id);
+    assert.isTrue(
+      res.customTheme?.colorScheme === "dark" ||
+        res.customTheme?.colorScheme === "light"
+    );
+    assert.isString(res.customTheme?.title);
+    assert.isString(res.customTheme?.author.username);
+
+    log(
+      `Theme: ${res.customTheme?.title} by ${res.customTheme?.author.username} (${res.customTheme?.colorScheme})`
+    );
+  },
+  "getCurrentThemeValues gets the current theme values": async () => {
+    const res = await themes.getCurrentThemeValues();
+
+    assert.isObject(res);
+    assert.isTrue(Object.values(res).every((t) => typeof t === "string"));
+  },
+  "onThemeChange fires when the theme changes": async (log) => {
+    log("⚠️ Please change your theme to run this test ⚠️");
+
+    await new Promise<void>(async (resolve) => {
+      const dispose = await themes.onThemeChange(() => {
+        dispose();
+        resolve();
+      });
+    });
+  },
+  "onThemeChangeValues fires when the theme changes": async (log) => {
+    log("⚠️ Please change your theme to run this test ⚠️");
+
+    await new Promise<void>(async (resolve) => {
+      const dispose = await themes.onThemeChangeValues((values) => {
+        assert.isObject(values);
+        assert.isTrue(
+          Object.values(values).every((t) => typeof t === "string")
+        );
+
+        dispose();
+        resolve();
+      });
+    });
   },
 };
 
