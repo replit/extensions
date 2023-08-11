966b07b9f7efe8524f7cd6b59871cc1f2a7a2937
Arnav Bansal <lunaroyster@users.noreply.github.com>
2023-08-12 00:18:28 +0530

    Fixed logging (#93)
    * fixed logging
    
    * fixed duplicate log
    
    * added more serializable types
    
    * fix types
    
    * version bump

--- modules/extensions/src/api/debug.ts
+++ modules/extensions/src/api/debug.ts
@@ -14,12 +14,29 @@ export type Serializable = ObjectType | Primitive | NumericIndexType;
 
 export type Data = Record<string, Serializable>;
 
+function isSerializable(thing: any): thing is Serializable {
+  if (["string", "number", "boolean", "undefined"].includes(typeof thing)) {
+    return true;
+  }
+
+  if (thing === null) {
+    return true;
+  }
+
+  return false;
+}
+
 /**
  * Logs information to the Extension Devtools
  */
 async function info(message: string, data?: Data) {
-  if (typeof message !== "string") {
-    throw new Error("Message must be a string");
+  if (!isSerializable(message)) {
+    // if someone uses console.info / console.log, the wrapper defined in index.ts will log the object to the console.
+    extensionPort.debug.warn(
+      "Attempted to log non-serializable message. See your browser devtools to access the logged object."
+    );
+
+    return;
   }
 
   return await extensionPort.debug.info(message, data);
@@ -29,8 +46,13 @@ async function info(message: string, data?: Data) {
  * Logs a warning to the extension devtools
  */
 async function warn(message: string, data?: Data) {
-  if (typeof message !== "string") {
-    throw new Error("Message must be a string");
+  if (!isSerializable(message)) {
+    // if someone uses console.warn, the wrapper defined in index.ts will log the object to the console.
+    extensionPort.debug.warn(
+      "Attempted to log non-serializable message. See your browser devtools to access the logged object."
+    );
+
+    return;
   }
 
   return await extensionPort.debug.warn(message, data);
@@ -40,8 +62,13 @@ async function warn(message: string, data?: Data) {
  * Logs an error message to the extension devtools
  */
 async function error(message: string, data?: Data) {
-  if (typeof message !== "string") {
-    throw new Error("Message must be a string");
+  if (!isSerializable(message)) {
+    // if someone uses console.error, the wrapper defined in index.ts will log the object to the console.
+    extensionPort.debug.warn(
+      "Attempted to log non-serializable message. See your browser devtools to access the logged object."
+    );
+
+    return;
   }
 
   return await extensionPort.debug.error(message, data);
