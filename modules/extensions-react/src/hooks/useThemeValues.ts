bc422b50c4441866cc82f720f6ca5526c9c128ff
Devin Halladay <devin@devinhalladay.com>
2023-09-05 13:24:14 -0400

    Add setCssVariables feature to useThemeValues hook

--- modules/extensions-react/src/hooks/useThemeValues.ts
+++ modules/extensions-react/src/hooks/useThemeValues.ts
@@ -2,16 +2,42 @@ import { useState } from "react";
 import { ThemeValuesGlobal } from "@replit/extensions";
 import useReplitEffect from "./useReplitEffect";
 
+const toKebabCase = (str: string) =>
+  str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+
+const buildCssString = (themeValues: ThemeValuesGlobal) => {
+  return Object.entries(themeValues)
+    .map(([key, value]) => `--${toKebabCase(key)}: ${value};`)
+    .join(" ");
+};
+
+const applyTheme = (css: string) => {
+  const styleElement = document.createElement("style");
+  styleElement.textContent = `:root { ${css} }`;
+  document.head.append(styleElement);
+};
+
 /**
  * Returns the global tokens of the current user's theme.
+ * @param setCssVariables If true, will add themed tokens as CSS variables to the :root selector.
  */
-export default function useThemeValues() {
+export default function useThemeValues({
+  setCssVariables = true,
+}: {
+  /** If true, will add themed tokens as CSS variables to the :root selector */
+  setCssVariables?: boolean;
+}) {
   const [values, setValues] = useState<ThemeValuesGlobal | null>(null);
 
   useReplitEffect(async ({ themes }) => {
     const themeValues = await themes.getCurrentThemeValues();
 
     setValues(themeValues);
+
+    if (setCssVariables) {
+      const css = buildCssString(themeValues);
+      applyTheme(css);
+    }
 
     await themes.onThemeChangeValues(setValues);
   }, []);
