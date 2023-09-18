e9745a50bb81939436ad10c70813a33380ff008f
lunaroyster <oneinaquadrillion@live.com>
2023-09-18 16:17:48 +0530

    fixes

--- modules/extensions-react/src/hooks/useIsExtension.ts
+++ modules/extensions-react/src/hooks/useIsExtension.ts
@@ -4,15 +4,8 @@ import { useReplit } from "./useReplit";
 /**
  * Returns whether your application is an extension once the handshake between Replit an your extension is established.  When the handshake is loading, returns `undefined`.
  */
-export default function useIsExtension(): undefined | boolean {
+export default function useIsExtension(): boolean {
   const { status } = useReplit();
 
-  switch (status) {
-    case HandshakeStatus.Ready:
-      return true;
-    case HandshakeStatus.Error:
-      return false;
-    default:
-      return undefined;
-  }
+  return status === HandshakeStatus.Ready;
 }
