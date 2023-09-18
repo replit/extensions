e857d28097d737f6161e0a7b910f1be43b6a9531
lunaroyster <oneinaquadrillion@live.com>
2023-09-18 16:19:09 +0530

    fixes

--- modules/dev/src/App.tsx
+++ modules/dev/src/App.tsx
@@ -3,7 +3,7 @@ import { useReplit } from "@replit/extensions-react";
 import "./App.css";
 
 export default function App() {
-  const { replit, status } = useReplit()
+  const { replit, status } = useReplit();
 
   const connected = status === HandshakeStatus.Ready;
 
--- modules/dev/src/index.tsx
+++ modules/dev/src/index.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
