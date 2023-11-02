c1e9513d10cefc4e2eac2f244488449a84a0800a
lunaroyster <oneinaquadrillion@live.com>
2023-11-03 02:10:19 +0530

    fix link

--- modules/extensions/src/api/me.ts
+++ modules/extensions/src/api/me.ts
@@ -1,7 +1,7 @@
 import { extensionPort } from "../util/comlink";
 
 /**
- * Returns the path to the current file the extension is opened with, if it is a [File Handler](/extensions/key-concepts#file-handler).
+ * Returns the path to the current file the extension is opened with, if it is a [File Handler](/extensions/basics/key-concepts#file-handler).
  */
 export function filePath() {
   return extensionPort.filePath;
