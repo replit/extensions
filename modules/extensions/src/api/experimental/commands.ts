5f0d77f79abb26dbbc02c5a1def0f77572785faf
Arnav Bansal <lunaroyster@users.noreply.github.com>
2023-11-07 02:39:36 +0530

    [commands] add contribution types (#106)
    * [commands] add contribution types
    
    * fix lint

--- modules/extensions/src/api/experimental/commands.ts
+++ modules/extensions/src/api/experimental/commands.ts
@@ -1,19 +1,5 @@
 import { extensionPort } from "../../util/comlink";
-import { CreateCommand, CommandProxy, Command } from "../../commands";
-
-export function register(command: CommandProxy): void {
-  extensionPort.experimental.commands.registerCommand(command);
-}
-
-export function registerCreate(
-  data: { commandId: string; contributions: Array<string> },
-  createCommand: CreateCommand
-): void {
-  extensionPort.experimental.commands.registerCreateCommand(
-    data,
-    createCommand
-  );
-}
+import { CreateCommand, CommandProxy, ContributionType } from "../../commands";
 
 export interface AddCommandArgs {
   /**
@@ -24,7 +10,7 @@ export interface AddCommandArgs {
   /**
    * The surfaces that this command should appear in. This is an array of strings
    */
-  contributions: Array<string>;
+  contributions: Array<ContributionType>;
 
   /**
    * A Command, or, a function that returns a Command.
@@ -32,12 +18,45 @@ export interface AddCommandArgs {
   command: CommandProxy | CreateCommand;
 }
 
+/**
+ * Adds a command to the command system.
+ *
+ * @param id The command's unique identifier. This is used to identify the command in Replit's command system
+ * @param contributions The surfaces that this command should appear in. This is an array of strings
+ * @param command A Command, or, a function that returns a Command.
+ */
 export function add({ id, contributions, command }: AddCommandArgs) {
   if (typeof command === "function") {
-    registerCreate({ commandId: id, contributions }, command);
+    extensionPort.experimental.commands.registerCreateCommand(
+      { commandId: id, contributions },
+      command
+    );
   } else {
-    registerCreate({ commandId: id, contributions }, async () => ({
-      ...command,
-    }));
+    extensionPort.experimental.commands.registerCreateCommand(
+      { commandId: id, contributions },
+      async () => ({
+        ...command,
+      })
+    );
   }
+}
+
+/**
+ * @deprecated Use `add` instead
+ */
+export function register(command: CommandProxy): void {
+  extensionPort.experimental.commands.registerCommand(command);
+}
+
+/**
+ * @deprecated Use `add` instead
+ */
+export function registerCreate(
+  data: { commandId: string; contributions: Array<string> },
+  createCommand: CreateCommand
+): void {
+  extensionPort.experimental.commands.registerCreateCommand(
+    data,
+    createCommand
+  );
 }
