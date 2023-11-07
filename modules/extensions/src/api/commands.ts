c9ffe2d1cad9b23fe503283c366f9d5fc620bf03
lunaroyster <oneinaquadrillion@live.com>
2023-11-08 04:37:31 +0530

    fixed exports

--- modules/extensions/src/api/commands.ts
+++ modules/extensions/src/api/commands.ts
@@ -34,7 +34,7 @@ export interface AddCommandArgs {
  * @param contributions The surfaces that this command should appear in. This is an array of strings
  * @param command A Command, or, a function that returns a Command.
  */
-export function add({ id, contributions, command }: AddCommandArgs) {
+function add({ id, contributions, command }: AddCommandArgs) {
   if (typeof command === "function") {
     let createCommand = proxy(async (cmdFnArgs: CommandFnArgs) => {
       const cmd = await command(cmdFnArgs);
@@ -62,30 +62,4 @@ export function add({ id, contributions, command }: AddCommandArgs) {
   }
 }
 
-/**
- * @deprecated Use `add` instead
- */
-export function register(command: CommandProxy): void {
-  extensionPort.commands.registerCommand(command);
-}
-
-/**
- * @deprecated Use `add` instead
- */
-export function registerCreate(
-  data: { commandId: string; contributions: Array<string> },
-  createCommand: CreateCommand
-): void {
-  extensionPort.commands.registerCreateCommand(
-    data,
-    async (args: CommandFnArgs) => {
-      const cmd = await createCommand(args);
-
-      if (!cmd) {
-        return null;
-      }
-
-      return isCommandProxy(cmd) ? cmd : Command(cmd);
-    }
-  );
-}
+export { add };
